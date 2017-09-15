08487c3f4294de0dabc8c99171a98d78d73b5068
Fabio Spampinato <spampinabio@gmail.com>
2017-09-15 21:54:55 +0200

    Options: added a basic page

--- template/src/options/components/options.tsx
+++ template/src/options/components/options.tsx
@@ -5,7 +5,12 @@ import * as React from "react";
 
 /* OPTIONS */
 
-const Options = () => <div>Options</div>;
+const Options = () => (
+  <div id="options">
+    <h1>{{displayName}}</h1>
+    <p>{{description}}</p>
+  </div>
+);
 
 /* EXPORT */
 

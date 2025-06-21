c3cf3843e7b74535f63229411263f0b5eaf18ecd
Fabio Spampinato <spampinabio@gmail.com>
2025-06-21 15:04:44 +0100

    Generating only a 256x256 icon, as the extension store seems to not need the others

--- template/vite.ts
+++ template/vite.ts
@@ -32,8 +32,7 @@ const config = defineConfig ({
     {
       name: 'copy:assets',
       writeBundle: async () => {
-        await fs.mkdir ( 'dist/resources/icon', { recursive: true } );
-        await fs.cp ( 'resources/icon', 'dist/resources/icon', { recursive: true } );
+        await fs.cp ( 'resources/icon/icon-256.png', 'dist/resources/icon/icon-256.png', { recursive: true } );
         await fs.cp ( 'manifest.json', 'dist/manifest.json' );
       }
     }
